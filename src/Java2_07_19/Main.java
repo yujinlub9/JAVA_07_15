ae51b619d62350e581050832aebacf8bd1743281
yujinlub9 <yujinlub9@gmail.com>
2021-07-19 12:02:03 +0900

    입력된 게시물 정보 저장

---
+++ src/Java2_07_19/Main.java
@@ -0,0 +1,67 @@
+package Java2_07_19;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("== 프로그램 시작 ==");
+
+		Scanner sc = new Scanner(System.in);
+
+		int lastArticleId = 0;
+
+		List<Article> articles = new ArrayList<>();
+
+		while (true) {
+			System.out.printf("명령어) ");
+			String command = sc.nextLine();
+
+			command = command.trim();
+
+			if (command.length() == 0) {
+				continue;
+			}
+
+			if (command.equals("system exit")) {
+				break;
+			}
+			if (command.equals("article write")) {
+				int id = lastArticleId + 1;
+				lastArticleId = id;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+				
+				Article article = new Article(id, title, body);
+				articles.add(article);
+
+				System.out.printf("%d번글이 생성되었습니다.\n", id);
+
+			} else if (command.equals("article list")) {
+				System.out.println("게시물이 없습니다.");
+				
+			} else {
+				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", command);
+			}
+		}
+
+		sc.close();
+
+		System.out.println("== 프로그램 끝 ==");
+	}
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
+
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+}
